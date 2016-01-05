2d0a0a67e546690bf0e7334906532b79cddedb14
Stephen Edwards <stephen.edwards@foostix.lu>
2016-01-05 19:54:37 +0100

    more work

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( foundation-icons/foundation-icons.eot )
+Rails.application.config.assets.precompile += %w( foundation-icons/foundation-icons.woff )
+Rails.application.config.assets.precompile += %w( foundation-icons/foundation-icons.ttf )
+Rails.application.config.assets.precompile += %w( foundation-icons/foundation-icons.svg )
