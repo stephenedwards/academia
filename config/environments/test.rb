f3d62206238883926e5c4ae62733331fb8e68912
Stephen Edwards <stephen.edwards@foostix.lu>
2016-01-19 10:56:58 +0100

    add smtp settings for prod and test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,5 +38,13 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  # config.action_view.raise_on_missing_translations = true#SMTP
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "jp.piazzolla@academia.lu", 
+    :password => "jeanpaul123", 
+    :authentication  => :login
+  }
 end
